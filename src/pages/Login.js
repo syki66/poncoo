898ad982bb361d6e6d9e6ff6719096b85c48136f
syki66 <66syki@gmail.com>
2022-08-25 22:07:06 +0900

    feat: update icon, title

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>Login</div>
+      <Link to='/1'>go Coupon</Link>
+    </>
+  );
 }
